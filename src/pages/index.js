abe83475476af19f118d00dfa8dffcd61b422daf
Alonzo Alden <alonzoalden@gmail.com>
2021-05-21 16:22:52 -0700

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,10 +27,10 @@ import LandingPageHeader from "../components/LandingPageHeader"
 import scrollTo from 'gatsby-plugin-smoothscroll'
 import MainNavbar from "../components/NavbarMain"
 import DarkFooter from "../components/DarkFooter"
-import ZoomMeetingImage from "../assets/flyers/WGFS Estate Financial Planning Zoom Workshop Flyer - may2021.png"
+import ZoomMeetingImage from "../assets/flyers/WGFS Estate Financial Planning Zoom Workshop June21.png"
 import ZoomMeetingImage2 from "../assets/flyers/WGFS Financial Empowerment Flyer June-1-21.png"
 
-const rsvpURL = "https://forms.gle/7C6xqvZbCsdDDsQK7";
+const rsvpURL = "https://forms.gle/GQ8KcNg9hBsA2f149";
 const rsvpURL2 = "https://forms.gle/mj3T7UXJgdEFREpaA";
 const IndexPage = () => (
   <Layout>
