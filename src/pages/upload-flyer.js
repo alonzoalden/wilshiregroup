f145fdf33f36359ae8965b1441667e0ed0ff463d
Alonzo Alden <alonzoalden@gmail.com>
2023-07-13 18:55:35 -0700

    Upload Flyer page UI updates

--- src/pages/upload-flyer.js
+++ src/pages/upload-flyer.js
@@ -6,75 +6,124 @@ import axios from "axios"
 import Logo from "../assets/images/wilshirelogo.png"
 import { Button, Container } from "reactstrap"
 import { FaImage } from "react-icons/fa";
-import JSONLinkData from "../../../assets/WGFS-link.json"
+import { FaPlusCircle } from "react-icons/fa";
+import {
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+} from 'reactstrap';
+import { right } from "@popperjs/core"
+
+const Comp = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+    return (<Dropdown isOpen={dropdownOpen} toggle={toggle} >
+        <DropdownToggle caret size="sm">Options</DropdownToggle>
+        <DropdownMenu>
+            <DropdownItem>Clear</DropdownItem>
+            <DropdownItem>Remove</DropdownItem>
+        </DropdownMenu>
+    </Dropdown>)
+}
 
 const UploadFlyerPage = (props) => {
 
-    React.useEffect(() => {
+    const flyerObj = { flyer: undefined, link: '', flyerUrl: undefined };
 
-        const images = require.context('../../../assets', true, /\.(png|jpg|jpeg)$/);
-
-        let initialPreviewState = {};
-        images.keys().forEach((elem) => {
-
-            const name = elem.split('WGFS-')[1].split('.')[0];
-
-            initialPreviewState = {
-                ...initialPreviewState,
-                [name]: images(`${elem}`).default
-            }
-        })
-
-        setPreviewState({
-            ...initialPreviewState
-        })
-
-    }, []);
-
-
-
+    const [formState2, setFormState2] = useState({
+        data: [flyerObj]
+    });
     const [serverState, setServerState] = useState({
         submitting: false,
         status: null
     });
-    const [formState, setFormState] = useState({
-        flyer1: undefined,
-        flyer2: undefined,
-        link1: JSONLinkData && JSONLinkData.links[0] || '',
-        link2: JSONLinkData && JSONLinkData.links[1] || ''
-    });
-    const [previewState, setPreviewState] = useState({
-        flyer1: '',
-        flyer2: ''
-    });
     const [formComplete, setFormComplete] = useState(false);
-
-    const hiddenFileInput = {
-        flyer1: React.useRef(null),
-        flyer2: React.useRef(null)
-    }
+    const [addNewEnabled, setAddNewEnabled] = useState(true);
+    const hiddenFileInputRefs = React.useRef([]);
 
     React.useEffect(() => {
 
-        const hasEmptyValue = Object.values(formState).some(val => !val);
+        axios({
+            method: "get",
+
+            // prod mode
+            // url: "https://wilshiregfs.com/api/photo",
+            url: "https://alonzoalden.com/api/photo",
+
+            // dev mode
+            // url: "http://localhost:3999/api/photo",
+
+        }).then(({ data }) => {
+
+            if (data && data.length) {
+
+                setFormState2({ data });
+
+            }
+
+        }).catch(response => {
+
+            console.log(response);
+
+        });
+
+    }, []);
+
+    React.useEffect(() => {
+
+        // Check if works
+        const hasEmptyValue = Object.values(formState2).some(val => !val);
         setFormComplete(!hasEmptyValue)
 
-    }, [formState]);
 
-    const handleClick = event => {
+    }, [formState2]);
 
-        const { name } = event.target;
-        hiddenFileInput[name].current.click();
+    const handleClick = (_, index) => {
+
+        hiddenFileInputRefs.current[index].click();
 
     };
 
-    const onInputChange = event => {
 
-        const { name, value } = event.target;
 
-        setFormState({
-            ...formState,
-            [name]: value
+    const handleOnSubmit = e => {
+
+        e.preventDefault();
+        const form = e.target;
+        setServerState({ submitting: true });
+
+        const formData = new FormData();
+
+        formState2.data.forEach((data) => {
+
+            if (data.flyer && data.link) {
+
+                formData.append('file', data.flyer);
+                formData.append('link', data.link);
+
+            }
+
+        })
+
+        axios({
+            method: "post",
+
+            // prod mode
+            // url: "https://wilshiregfs.com/api/photo",
+            url: "https://alonzoalden.com/api/photo",
+
+            // dev mode
+            // url: "http://localhost:3999/api/photo",
+            data: formData
+        }).then(r => {
+
+            handleServerResponse(true, "Thanks!", form);
+
+        }).catch(r => {
+
+            handleServerResponse(false, r.response, form);
+
         });
 
     };
@@ -83,7 +132,7 @@ const UploadFlyerPage = (props) => {
 
         setServerState({
             submitting: false,
-            // status: { ok, msg }
+            status: { ok, msg }
         });
         if (ok) {
             form.reset();
@@ -91,82 +140,107 @@ const UploadFlyerPage = (props) => {
 
     };
 
-    const handleOnSubmit = e => {
+    const isAddNewButtonEnabled = () => {
 
-        e.preventDefault();
-        const form = e.target;
-        setServerState({ submitting: true });
-        setFormState({
-            ...formState,
-        })
+        const lastItem = formState2.data[formState2.data.length - 1];
 
-        const formData = new FormData();
-        formData.append('file', formState.flyer1);
-        formData.append('file', formState.flyer2);
-        formData.append('link', formState.link1);
-        formData.append('link', formState.link2);
-        formData.append('updateFiles', []);
+        const lastItemCompleted = (formState2.data && formState2.data.length < 2) || (lastItem.flyerUrl && lastItem.link);
+        const enabled = lastItemCompleted && formState2.data.length < 8;
+        setAddNewEnabled(!!enabled);
+        return !!enabled;
 
-        const config = {
-            headers: { 'content-type': 'multipart/form-data' }
+    }
+
+    const onAddNewFlyer = () => {
+
+        if (isAddNewButtonEnabled()) {
+
+            const data = [...formState2.data];
+            data.push(flyerObj)
+            setFormState2({ data })
+
         }
-        axios({
-            config,
-            method: "post",
 
-            // prod mode
-            // url: "https://wilshiregfs.com/api/photo",
+    }
 
-            // dev mode
-            url: "http://localhost:3999/api/photo",
-            data: formData
+    const onInputChange = (event, index) => {
+
+        const { value } = event.target;
+        const item = formState2.data[index];
+        const newItem = {
+            ...item,
+            link: value,
+        };
+        const newData = [...formState2.data];
+        newData[index] = newItem;
+
+        setFormState2({
+            data: newData
         })
-            .then(r => {
-                handleServerResponse(true, "Thanks!", form);
-            })
-            .catch(r => {
-                handleServerResponse(false, r.response, form);
-            });
 
     };
-    const onSelectFile = event => {
+
+    const onSelectFile = (event, index) => {
 
         if (!event.target.files || event.target.files.length === 0) {
 
-            setFormState({
-                ...formState,
-                [name]: undefined
-            });
-            setPreviewState({
-                ...previewState,
-                [name]: undefined
-            });
+            const item = formState2.data[index];
+            const newItem = {
+                flyer: undefined,
+                link: item.link,
+                flyerUrl: undefined
+            };
+            const newData = [...formState2.data];
+            newData[index] = newItem;
+
+
+            setFormState2({
+                data: newData
+            })
             return;
 
         }
 
         const { name } = event.target;
         const image = event.target.files[0];
-        const renamedImage = new File([image], `${name}.png`, {
+        const renamedImage = new File([image], `flyer${index + 1}.png`, {
             type: image.type,
-          });
-
-        const preview = URL.createObjectURL(renamedImage);
-
-        setFormState({
-            ...formState,
-            [name]: renamedImage
         });
 
-        setPreviewState({
-            ...previewState,
-            [name]: preview
+        const flyerUrl = URL.createObjectURL(renamedImage);
+
+        const newItem = {
+            flyer: renamedImage,
+            link: formState2.data[index].link,
+            flyerUrl
+        };
+        const newData = [...formState2.data];
+        newData[index] = newItem;
+
+        setFormState2({
+            data: newData
         })
 
     }
 
+    const onRemove = (_, index) => {
+
+        const confirmation = window.confirm("Are you sure you want to remove this section?");
+        if (confirmation) {
+
+            const data = [...formState2.data];
+            data.splice(index, 1);
+
+            setFormState2({
+                data
+            });
+
+        }
+
+    }
     const style = { color: "#dadada", fontSize: "64px" };
-    // const styleCheck = { color: "0d6efd", fontSize: "12px" };
+    const styleAddButton = { color: "#dadada", fontSize: "128px" };
+
     return (
         <Layout>
             <div className="logo">
@@ -174,97 +248,76 @@ const UploadFlyerPage = (props) => {
                 <span>Hello, Alonzo </span>
             </div>
             <Container>
-                {!serverState.status ? <>
+                {<>
                     <div className="upload-page-container">
                         <h1>WGFS: Upload Flyers</h1>
-                        <h5>Please enter a link to the meeting and upload a flyer for each section.</h5>
+                        <h5>Please enter a link to the meeting and upload an image, or add a new flyer.</h5>
+                        <Button
+                            color="primary"
+                            outline
+                            onClick={onAddNewFlyer}
+                            style={{ marginRight: '5px' }}
+                        >
+                            Add Flyer
+                        </Button>
+                        { addNewEnabled ? '' :  <span>Please complete all flyers before adding a new one.</span> }
                         <form>
                             <div className="upload-section-container">
-                                <div className="upload-section">
-                                    <h3>Flyer 1</h3>
-                                        <div className="form-group">
-                                            <label htmlFor="exampleInputName">Meeting Link</label>
-                                            <input type="text" name="link1" className="form-control"
-                                                value={formState.link1}
-                                                placeholder="Enter the link to the meeting"
-                                                onChange={onInputChange}
-                                                required
-                                                />
-                                        </div>
-                                        <label>Meeting Image</label>
-                                        { previewState.flyer1 ? <img className="preview-img" src={previewState.flyer1} /> :
-                                            <div className="placeholder-image"><FaImage style={style} /><div>Please select a flyer</div></div>
-                                        }
-                                        <Button className="btn btn-outline" name="flyer1" onClick={handleClick} >
-                                            { previewState.flyer1 ? 'Change' : 'Select' }
-                                        </Button>
-                                        <input
-                                            type="file"
-                                            name="flyer1"
-                                            accept="image/png, image/gif, image/jpeg"
-                                            ref={hiddenFileInput.flyer1}
-                                            onChange={onSelectFile}
-                                            style={{display: 'none'}}
-                                            required
-                                        />
-                                        {serverState.status && (
-                                            <p className={!serverState.status.ok ? "errorMsg" : ""}>
-                                                {serverState.status.msg}
-                                            </p>
-                                        )}
-                                </div>
-                                <div className="upload-section">
-                                    <h3>Flyer 2</h3>
-                                        <div className="form-group">
-                                            <label htmlFor="exampleInputName">Meeting Link</label>
-                                            <input type="text" name="link2" className="form-control"
-                                                value={formState.link2}
-                                                placeholder="Enter the link to the meeting"
-                                                onChange={onInputChange}
-                                                required
-                                                 />
-                                        </div>
-                                        {/* <label>Meeting Image { selectedFile && <FaCheck style={styleCheck}/> }</label>  */}
-                                        <label>Meeting Image </label>
+                                {(() => {
+                                    return formState2.data.map((data, i) => (
 
-                                        { previewState.flyer2 ? <img className="preview-img"src={previewState.flyer2} /> :
-                                        <div className="placeholder-image"><FaImage style={style} /><div>Please select a flyer</div></div>
-                                        }
-                                        <Button className="btn btn-outline" name="flyer2" onClick={handleClick} disabled={serverState.submitting} >
-                                            { previewState.flyer2 ? 'Change' : 'Select' }
-                                        </Button>
-                                        <input
-                                            type="file"
-                                            name="flyer2"
-                                            accept="image/png, image/gif, image/jpeg"
-                                            ref={hiddenFileInput.flyer2}
-                                            onChange={onSelectFile}
-                                            style={{display: 'none'}}
-                                            required
-                                        />
-                                        {serverState.status && (
-                                            <p className={!serverState.status.ok ? "errorMsg" : ""}>
-                                                {serverState.status.msg}
-                                            </p>
-                                        )}
-                                    </div>
-                                    </div>
+                                        <div className="upload-section" key={i}>
+
+                                            {/* <Comp /> */}
+
+                                            <h3>Flyer {i + 1}</h3>
+                                            <div className="form-group">
+                                                <label htmlFor="exampleInputName">Meeting Link</label>
+                                                <input type="text" name={'text-' + i} className="form-control"
+                                                    value={data.link}
+                                                    placeholder="Enter the link to the meeting"
+                                                    onChange={(event) => onInputChange(event, i)}
+                                                    required
+                                                />
+                                            </div>
+                                            <label>Meeting Image</label>
+                                            {data.flyerUrl ? <img className="preview-img" src={data.flyerUrl} /> :
+                                                <div className="placeholder-image"><FaImage style={style} /><div>Please select a flyer</div></div>
+                                            }
+                                            <Button className="btn btn-outline" onClick={(event) => handleClick(event, i)} >
+                                                {data.flyerUrl ? 'Change' : 'Select'}
+                                            </Button>
+                                            <input
+                                                type="file"
+                                                name={'file-' + i}
+                                                accept="image/png, image/gif, image/jpeg"
+                                                ref={el => hiddenFileInputRefs.current[i] = el}
+                                                onChange={(event) => onSelectFile(event, i)}
+                                                style={{ display: 'none' }}
+                                                required
+                                            />
+                                            <div className="mt-2"><Button color="link" onClick={(event) => onRemove(event, i) }>Remove</Button></div>
+                                        </div>
+
+                                    ))
+
+                                })()}
+
+                                {/* <div className="upload-section placeholder-add-button">
+                                    <FaPlusCircle style={styleAddButton} onClick={onAddNewFlyer} /><div>{addNewEnabled ? 'Add a new flyer' : 'Please finish last flyer to add a new one'} </div>
+                                </div> */}
+                            </div>
+
                             <div>
-                                    { formComplete && <h5 className="mt-4">If everything looks correct, please save.</h5> }
-                                    <Button className="btn btn-primary btn-lg" onClick={handleOnSubmit} type="submit" disabled={serverState.submitting}>
-                                        { serverState.submitting ? 'Saving...' : 'Save' }
-                                    </Button>
-                                </div>
-                            </form>
+                                {formComplete && <h5 className="mt-4">This is the order the flyers will appear on the website. If everything looks correct, please save.</h5>}
+                                <Button className="btn btn-primary btn-lg" onClick={handleOnSubmit} type="submit" disabled={serverState.submitting}>
+                                    {serverState.submitting ? 'Saving...' : 'Save'}
+                                </Button>
+                            </div>
+                        </form>
 
                     </div>
-                </>
-                    :
-                    <>
-                        <div className="mt-5"><a href="https://wilshiregfs.com" target="blank"><img src={Logo} /></a></div>
-                        <h4 className="mt-5">Thank you, We appreciate your review.</h4>
-                    </>
-                }
+                </>}
             </Container>
         </Layout>
     )
