0d693b5e6f66e9c4ee29097693e7dbfb2960b686
Alonzo Alden <alonzoalden@gmail.com>
2023-07-15 14:41:05 -0700

    mobile view updates, prod mode env

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -17,11 +17,11 @@ function UpcomingEvents() {
             method: "get",
 
             // prod mode
-            //url: "https://wilshiregfs.com/api/photo",
+            url: "https://wilshiregfs.com/api/photo",
             // url: "https://alonzoalden.com/api/photo",
 
             // dev mode
-            url: "http://localhost:3999/api/photo",
+            // url: "http://localhost:3999/api/photo",
 
         }).then(({ data }) => {
 
